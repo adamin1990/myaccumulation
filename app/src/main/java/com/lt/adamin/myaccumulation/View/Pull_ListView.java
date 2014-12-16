25a47ba96969f30259d735b3da2e34bbe5f2ab36
adamin <adamin1990@gmail.com>
2014-12-16 09:26:05 +0800

    ListView 的弹簧效果

---
+++ app/src/main/java/com/lt/adamin/myaccumulation/View/Pull_ListView.java
@@ -0,0 +1,83 @@
+package com.lt.adamin.myaccumulation.View;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.ListView;
+
+/**
+ * listView 滑动 
+ * Created by adamin on 2014/12/16.
+ */
+
+public class Pull_ListView extends ListView implements  Runnable {
+    private float mLastDownY = 0f;
+    private int mDistance = 0;
+    private int mStep = 10;
+    private boolean mPositive = false;
+
+    public Pull_ListView(Context context) {
+        super(context);
+    }
+
+    public Pull_ListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Pull_ListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if (mLastDownY == 0f && mDistance == 0) {
+                    mLastDownY = ev.getY();
+                    return true;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                if (mDistance != 0) {
+                    mStep = 1;
+                    mPositive = (mDistance >= 0);   //判断上下拉动
+                    this.post(this);
+                    return true;
+                }
+                mLastDownY = 0f;
+                mDistance = 0;
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (mLastDownY != 0f) {
+                    mDistance = (int) (mLastDownY - ev.getY());
+                    if ((mDistance < 0 && getFirstVisiblePosition() == 0 && getChildAt(0).getTop() == 0) || (mDistance > 0 && getLastVisiblePosition() == getCount() - 1)) {
+                        mDistance /= 2;
+                        scrollTo(0, mDistance);
+                        return true;
+                    }
+                }
+                mDistance = 0;
+                break;
+
+
+
+        }
+        return super.onTouchEvent(ev);
+    }
+
+    @Override
+    public void run() {
+        mDistance += mDistance > 0 ? -mStep : mStep;
+        scrollTo(0, mDistance);
+        if ((mPositive && mDistance <= 0) || (!mPositive && mDistance >= 0)) {
+            scrollTo(0, 0);
+            mDistance = 0;
+            mLastDownY = 0f;
+            return;
+        }
+        mStep += 1;
+        this.postDelayed(this, 10);
+    }
+}
